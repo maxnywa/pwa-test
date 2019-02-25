619ef2b104f47d34b5b98d012452d9eda0255e63
Asieieva_Maria <asieieva.maria@gmail.com>
2019-02-25 13:11:43 +0200

    deploy

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
-var express = require('express');
-var https = require('https');
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
+// var express = require('express');
+// var https = require('https');
+// var http = require('http');
+// var fs = require('fs');
+// var path = require('path');
+//
+// // This line is from the Node.js HTTPS documentation.
+// var options = {
+//   key: fs.readFileSync('test/key.pem'),
+//   cert: fs.readFileSync('test/cert.pem')
+// };
+//
+//
+//
+// // Create a service (the app object is just a callback).
+// var app = express();
+//
+// app.use(express.static(path.join(__dirname, 'dist/pwa')));
+//
+// app.get('*', (req, res)=>{
+//   res.sendFile(path.join(__dirname, 'dist/pwa/index.html'));
+// });
+//
+// // Create an HTTP service.
+// http.createServer(app).listen(3000);
+// // Create an HTTPS service identical to the HTTP service.
+// // https.createServer(options, app).listen(443, ()=> console.log('running HTTPS'));
+const http = require('http');
+const path = require('path');
+const express = require('express');
 
-// This line is from the Node.js HTTPS documentation.
-var options = {
-  key: fs.readFileSync('test/key.pem'),
-  cert: fs.readFileSync('test/cert.pem')
-};
+const app = express();
 
-
-
-// Create a service (the app object is just a callback).
-var app = express();
-
-app.use(express.static(path.join(__dirname, 'dist/pwa')));
+app.use(express.static(path.join(__dirname, 'dist/routingapp')));
 
 app.get('*', (req, res)=>{
-  res.sendFile(path.join(__dirname, 'dist/pwa/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/routingapp/index.html'));
 });
 
-// Create an HTTP service.
-http.createServer(app).listen(80);
-// Create an HTTPS service identical to the HTTP service.
-https.createServer(options, app).listen(443, ()=> console.log('running HTTPS'));
+const port = process.env.PORT|| '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, ()=> console.log('running'));
